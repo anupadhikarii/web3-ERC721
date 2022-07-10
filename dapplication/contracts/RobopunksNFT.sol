eae212d80f3defe6638839c566bcb1f98931030a
anup <aanupadhikari94@gmail.com>
2022-07-10 08:48:39 -0700

    inital

---
+++ dapplication/contracts/RobopunksNFT.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.9;
+
+
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+
+//0xA33ac1F1769c68Ae461C847F68C655b89eD18f96
+contract RobopunksNFT is ERC721,Ownable{
+
+    uint256 public mintPrice;
+    uint256 public totalSupply;
+    uint256 public maxSupply;
+    uint256 public maxPerWallet;
+    bool public isPublicMintEnable;
+    string internal baseTokenUri;
+    address  payable public withdrawWallet;
+
+    mapping(address => uint256) public walletMints;
+    
+
+    constructor() payable ERC721('RoboPunks','RP'){
+        mintPrice = 0.02 ether;
+        totalSupply =0;
+        maxSupply = 1000;
+        maxPerWallet = 3;
+    }
+
+    function setIsPublicMintEnabled(bool isPublicMintEnable_) external onlyOwner{
+        isPublicMintEnable = isPublicMintEnable_;
+
+    }
+
+    function setBaseTokenUri(string calldata baseTokenUri_) external onlyOwner{
+        baseTokenUri  = baseTokenUri_;
+    }
+
+    function tokenURI(uint tokenId_) public view override returns(string memory){
+        require(_exists(tokenId_), 'Token does not exist');
+        return string(abi.encodePacked(baseTokenUri, Strings.toString(tokenId_),".json"));
+
+    }
+
+    function withdraw() external onlyOwner{
+        (bool success,) =  withdrawWallet.call{ value: address(this).balance}('');
+        require(success,'withdraw failed');
+    }
+
+    function mint(uint256 quantity_)public payable{
+      
+        require(isPublicMintEnable,'minting is enable');
+        require(msg.value == quantity_ * mintPrice,'wrong mint value');  
+        require(totalSupply + quantity_ <= maxSupply, 'sold  out');
+        require(walletMints[msg.sender] + quantity_ <= maxPerWallet ,'exceed max wallets');
+
+        for(uint256 i = 0; i <quantity_; i++){
+            uint256 newTokenID = totalSupply +1;
+            totalSupply++;
+            _safeMint(msg.sender,newTokenID);
+        }
+        }
+}
---
+++ dapplication/hardhat.config.js
@@ -0,0 +1,33 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan")
+const dotenv = require("dotenv");
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ module.exports = {
+  solidity: "^0.8.9",
+  networks: {
+   rinkeby: {
+     url: process.env.REACT_APP_RINKEBE_RPC_URL, //Infura url with projectId
+     accounts: [process.env.RINKEBY_PRIVATE_KEY ] // add the account that will deploy the contract (private key)
+    },
+  },
+  etherscan:{
+    apiKey: process.env.ETHERSCAN_API
+  }
+};
